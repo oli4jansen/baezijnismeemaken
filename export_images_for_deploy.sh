12ecf81f36f3cd42405d1fb3c522b3147931ebd6
Olivier Jansen <olivier@spinque.com>
2023-04-04 13:01:53 +0200

    improve deploy script

--- export_images_for_deploy.sh
+++ export_images_for_deploy.sh
@@ -1,7 +1,10 @@
-# Remove images if existing
-docker rmi baezijnismeemaken_server baezijnismeemaken_client
+# Create deploy directory
+mkdir -p deploy
 
-# Rebuild images
+# Remove images if existing
+docker rmi baezijnismeemaken_server baezijnismeemaken_client --force
+
+# Rebuild images with fresh source code
 docker build -t baezijnismeemaken_server server
 docker build -t baezijnismeemaken_client client
 
@@ -11,10 +14,10 @@ docker save -o deploy/client.tar baezijnismeemaken_client
 
 # Copy .env file from server directory to deploy directory.
 # This file may be overwritten when actually deploying on the server
-cp server/.env deploy
+cp server/.env deploy/
 
 # Copy the docker-compose definition
-cp docker-compose.yml deploy
+cp docker-compose.yml deploy/
 
 # Provide an executable script to load the images and run compose
 rm -rf deploy/start.sh
@@ -22,3 +25,8 @@ echo "docker load -i client.tar" >> deploy/start.sh
 echo "docker load -i server.tar" >> deploy/start.sh
 echo "docker compose up" >> deploy/start.sh
 chmod +x deploy/start.sh
+
+# Zip the deploy files
+tar -czvf baezijnismeemaken-deploy.tar.gz deploy
+# Remove the directory
+# rm -rf deploy
